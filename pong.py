d01114e2d9841c3ed2d4f282fd7aaf73d25a09dd
Filia Medici <69800575+Filia-Medici-x86-64@users.noreply.github.com>
2020-11-01 15:44:50 -0600

    Update pong.py

--- pong.py
+++ pong.py
@@ -19,3 +19,55 @@ paddle_a = turtle.Turtle()
 paddle_a.speed(0)
 paddle_a.shape("square")
 paddle_a.colour("white")
+paddle_a.shapesize(stretch_wid=5,stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+
+# Paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.colour("white")
+paddle_b.shapesize(stretch_wid=5,stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
+
+# Ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.colour("white")
+ball.penup()
+ball.goto(0, 0)
+
+# Functions
+def paddle_a_up():
+  y = paddle_a.ycor()
+  y += 20
+  paddle_a.sety(y)
+
+def paddle_a_down():
+  y = paddle_a.ycor()
+  y -= 20
+  paddle_a.sety(y)
+
+def paddle_b_up():
+  y = paddle_b.ycor()
+  y += 20
+  paddle_b.sety(y)
+
+def paddle_b_down():
+  y = paddle_b.ycor()
+  y -= 20
+  paddle_b.sety(y)
+
+# Keyboard binding
+wn.listen()
+wn.onkeypress(paddle_a_up, "w")
+wn.onkeypress(paddle_a_down, "s")
+wn.onkeypress(paddle_b_up, "Up")
+wn.onkeypress(paddle_b_down, "Down")
+
+# Main game loop
+while True:
+  wn.update()
